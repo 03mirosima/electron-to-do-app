f351e75f1e207a9ce55ff5cfa8735f7e45d61189
03mirosima <sezenmiray2009@hotmail.com>
2021-02-14 17:44:38 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const electron = require("electron");
+const { ipcRenderer } = electron;
+const ul = document.querySelector("ul");
+
+ipcRenderer.on("item:add", function (e, item) {
+  const li = document.createElement("li");
+  const itemText = document.createTextNode(item);
+  li.appendChild(itemText);
+  ul.appendChild(li);
+});
+
+ipcRenderer.on("item:clear", function () {
+  ul.innerHTML = "";
+});
+
+ul.addEventListener("dblclick", removeItem);
+
+function removeItem(e) {
+  e.target.remove();
+}
---
+++ main.js
@@ -0,0 +1,103 @@
+const { app, BrowserWindow, Menu, ipcMain } = require("electron");
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      /*  worldSafeExecuteJavaScript: true,
+      contextIsolation: true */
+    },
+  });
+
+  mainWindow.loadFile("index.html");
+
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+
+  return mainWindow;
+}
+app.whenReady().then(createWindow);
+
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 300,
+    title: "Yapılacak Ekle",
+    webPreferences: {
+      nodeIntegration: true,
+      /*  worldSafeExecuteJavaScript: true,
+      contextIsolation: true */
+    },
+  });
+  addWindow.loadFile("./app/addToDo/addWindow.html");
+
+  return addWindow;
+}
+
+ipcMain.on("item:add", function (e, item) {
+  console.log(item);
+  mainWindow.webContents.send("item:add", item);
+  addWindow.close();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+const mainMenuTemplate = [
+  {
+    label: "Yapılacakları Düzenle",
+    submenu: [
+      {
+        label: "Yapılacak Ekle",
+        click() {
+          createAddWindow();
+        },
+      },
+      {
+        label: "Tüm Yapılacakaları Temizle",
+        click(){
+          mainWindow.webContents.send("item:clear")
+        }
+      },
+      {
+        label: "Uygulamayı Kapat",
+        accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+];
+
+if (process.env.NODE_ENV !== "production") {
+  mainMenuTemplate.push({
+    label: "Dev Tools",
+    click(item, focusedWindow) {
+      focusedWindow.toggleDevTools();
+    },
+    submenu: [
+      {
+        label: "Geliştirici Araçlar",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        },
+      },
+      {
+        label: "Tekrar Yükle",
+        role: "reload",
+      },
+    ],
+  });
+}
